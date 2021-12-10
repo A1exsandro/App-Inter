1b0bad543b9f255545ee075cc068bb97ede1f75e
A1exsandro <alexsandro.acp@gmail.com>
2021-12-10 15:48:55 -0400

    Token Config

--- node-template/src/resources/user/dtos/user.service.ts
+++ node-template/src/resources/user/dtos/user.service.ts
@@ -1,7 +1,9 @@
 import { getRepository } from 'typeorm';
 import md5 from 'crypto-js/md5'
-//import { sign } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
+
 import { User } from '../../../entity/User';
+import authConfig from '../../../config/auth';
 
 import { UserSignup } from './user.signup.dtos';
 import { UserSignin } from './user.signin.dtos';
@@ -22,7 +24,22 @@ export default class UseService {
             throw new AppError('Usuário não encontrado', 401);
         }
 
-        return existUser;
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({
+            firstName: existUser.firstName,
+            lastName: existUser.lastName,
+            accountNumber: existUser.accountNumber,
+            accountDigit: existUser.accountDigit,
+            wallet: existUser.wallet
+        }, secret, {
+            subject: existUser.id,
+            expiresIn,
+        });
+
+        //delete existUser.password
+
+        return {  acessToken: token }
     }
 
     async signup(user: UserSignup){
